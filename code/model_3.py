39f1267693d0a134aff023383abe157568c2e97c
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2018-06-16 15:11:16 +0530

    model 3 - Random Forest Regressor parameters tune

--- code/model_3.py
+++ code/model_3.py
@@ -1,58 +1,39 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
-from sklearn.feature_selection import SelectKBest, RFE, mutual_info_regression, mutual_info_classif, SelectFromModel
-from sklearn.feature_selection import chi2
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
-from sklearn.utils.testing import set_random_state
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from code import model_2_preprocess
 
-feature = pd.read_csv('../data/dengue_features_train.csv')
-label = pd.read_csv('../data/dengue_labels_train.csv')
-df = pd.merge(feature, label, how='outer', on=label.columns.tolist()[:-1])
+sj_train, iq_train, sj_label, iq_label = model_2_preprocess.preprocess('../data/dengue_features_train.csv',
+                                                                       '../data/dengue_labels_train.csv')
 
-np.sum(df.isnull(), axis=0)
-df = df.dropna(axis=0, thresh=20)
-np.sum(df.isnull(), axis=0)
-df.city.value_counts()
-df.fillna(method='ffill', inplace=True)
-df = df.fillna(df.mean())
+sj_x_train, sj_x_test, sj_y_train, sj_y_test = train_test_split(sj_train, sj_label['total_cases'], test_size=0.33,
+                                                                random_state=0, shuffle=False)
 
-df_sj = df[df.city == 'sj'].copy()
-df_iq = df[df.city == 'iq'].copy()
-df = df.drop(["city", "year", "weekofyear", "week_start_date"], axis=1)
-x_set = df.drop(["total_cases"], axis=1)
-y_set = df["total_cases"]
-print(x_set.columns.tolist())
+iq_x_train, iq_x_test, iq_y_train, iq_y_test = train_test_split(iq_train, iq_label['total_cases'], test_size=0.33,
+                                                                random_state=0, shuffle=False)
 
-# # Feature selection from Logistic Regression RFE
-# model = LogisticRegression()
-# rfe = RFE(model, 5)
-# fit = rfe.fit(x_set, y_set)
-# print(fit)
-# print("Num Features: ", fit.n_features_)
-# print("Selected Features: ", fit.support_)
-# print("Feature Ranking: ", fit.ranking_)
-#
-# # Importance of features
-# model = ExtraTreesClassifier()
-# model.fit(x_set, y_set)
-# print(model.feature_importances_)
+sj_model = RandomForestRegressor(n_estimators=840, max_depth=65, max_features=10, min_samples_split=70,
+                                 min_samples_leaf=3, criterion='mae', warm_start=True)
+sj_model.fit(sj_x_train, sj_y_train)
+sj_predict_value = sj_model.predict(sj_x_test)
 
-# mutual_info_classif
-# mutual = mutual_info_classif(x_set, y_set, n_neighbors=2, random_state=5)
-# print(mutual)
 
-# L1 based feature selection
-# lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(x_set, y_set)
-# model = SelectFromModel(lsvc, prefit=True)
-# X_new = model.transform(x_set)
-# print(x_set.shape)
-# print(X_new.shape)
+iq_model = RandomForestRegressor(n_estimators=751, max_features='auto', max_depth=76, min_samples_leaf=45,
+                                 criterion='mae', min_weight_fraction_leaf=0.1, warm_start=True, min_samples_split=4)
+iq_model.fit(iq_x_train, iq_y_train)
+iq_predict_value = iq_model.predict(iq_x_test)
 
-clf = Pipeline([
-  ('feature_selection', SelectFromModel(LinearSVC(penalty="l1", dual=False))),
-  ('classification', RandomForestClassifier())
-])
-print(clf.fit(x_set, y_set))
+print("SJ" + str(mean_absolute_error(sj_y_test, sj_predict_value)))
+print("IQ " + str(mean_absolute_error(iq_y_test, iq_predict_value)))
+
+sj_test, iq_test, sj_test_label, iq_test_label = model_2_preprocess.preprocess('../data/dengue_features_test.csv')
+
+sj_predictions = sj_model.predict(sj_test).astype(int)
+iq_predictions = iq_model.predict(iq_test).astype(int)
+
+submission = pd.read_csv("../data/submission_format.csv")
+submission.total_cases = np.concatenate([sj_predictions, iq_predictions])
+submission.to_csv("../result/submission_3.csv")
+
